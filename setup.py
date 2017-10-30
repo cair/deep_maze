4ca2179e8efcaa8523a6a145a7e54611deab1779
Per-Arne Andersen <per@sysx.no>
2017-10-30 17:42:59 +0100

    Fix for setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='gym_maze',
       version='0.0.1',
-      install_requires=['gym', 'cython', 'numpy', 'scipy', 'pygame']
+      install_requires=['gym', 'cython', 'numpy', 'scipy', 'pygame'],
+      packages=['gym_maze', 'gym_maze.envs']
       )
