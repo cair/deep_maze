774c78bb862cf3c49ac6d1183808cd592475d691
Per-Arne Andersen <perara12@gmail.com>
2017-12-05 11:37:54 +0100

    Updated state_space script

--- state_space.py
+++ state_space.py
@@ -3,34 +3,101 @@ from decimal import Decimal
 import plotly.offline as py
 import plotly.graph_objs as go
 import numpy as np
-
-
 # Create random data with numpy
 import numpy as np
-
 import operator as op
-
-
-
 import math
+import scipy
+import scipy.special
+
 
 def nCr(n, k):
     f = math.factorial
     return Decimal(f(n)) / Decimal(f(k)) * Decimal(f((n-k)))
 
-N = 55*55
+
+comb = scipy.special.comb(64, 32)
+
+N = 37*37
 x_values = np.arange(N)
 x_values = x_values[2:]
-y_values = [nCr(x, 2) for x in x_values]
+y_values = [scipy.special.comb(x, 2) for x in x_values]
+max_val = max(y_values)
 
-print(y_values)
+
+map_sizes = [
+    (7, 7),
+    (9, 9),
+    (11, 11),
+    (13, 13),
+    (15, 15),
+    (17, 17),
+    (19, 19),
+    (25, 25),
+    (35, 35),
+]
+
+
+
 
 # Create a trace
 trace = go.Scatter(
     x = x_values,
-    y = y_values
+    y = y_values,
+    name="Complexity"
 )
 
-data = [trace]
+trace1 = go.Scatter(
+    x=[x*y for x, y in map_sizes],
+    y=[max_val * 1.03 for x, y in map_sizes],
+    text=["%sx%s" % (x, y) for x, y in map_sizes],
+    mode="text",
+    name="Maze Size/N-states"
+)
 
-py.plot(data, filename='basic-line')
+trace2 = go.Scatter(
+    x=[(x*y) for x, y in map_sizes],
+    y=[max_val * 1.01 for x, y in map_sizes],
+    text=["%s" % int(scipy.special.comb(x*y, 2)) for x, y in map_sizes],
+    mode="text",
+    name="States",
+    showlegend=False
+)
+
+
+data = [trace, trace1, trace2]
+
+layout = {
+    'title': "Deep Maze State-Space",
+    'xaxis': {
+        'range': [0, N],
+        'showticklabels': False,
+        "title": "Maze Size"
+    },
+    'yaxis': {
+        'range': [0, max_val * 1.10],
+        'tickformat': '',
+        'title': "Complexity"
+    },
+    'shapes': [
+        # Line Vertical
+        {
+            'type': 'line',
+            'x0': x*y,
+            'y0': 0,
+            'x1': x*y,
+            'y1': max_val,
+            'line': {
+                'color': 'rgb(255, 0, 0)',
+                'width': 3,
+            },
+        } for x, y in map_sizes
+    ]
+}
+fig = {
+    'data': data,
+    'layout': layout,
+}
+py.plot(
+    fig,
+    filename='basic-line', )
