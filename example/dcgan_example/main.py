f716c0e275065ee1cabafa329b5ffbdd25570dff
Per-Arne Andersen <perara12@gmail.com>
2018-03-26 14:34:04 +0200

    UPdate

--- example/dcgan_example/main.py
+++ example/dcgan_example/main.py
@@ -3,9 +3,15 @@ from collections import namedtuple
 import gym
 import gym_maze
 import random
-
+import numpy as np
 from example.dcgan_example.explore.dream import Dream
 from example.dcgan_example.memory import ReplayMemory
+
+def rgb2gray(rgb):
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+    return gray
+
 
 if __name__ == "__main__":
 
@@ -25,7 +31,7 @@ if __name__ == "__main__":
         batch_size=16,
         screen_width=84,
         screen_height=84,
-        screen_dim=3,
+        screen_dim=1,
         action_size=4,
         cnn_format="N/A",
         memory_size=1000
@@ -41,12 +47,14 @@ if __name__ == "__main__":
         a = random.randint(0, 3)
         s1, r, terminal, info = env.step(a)
         env.render()
+        s1 = np.expand_dims(rgb2gray(s1), 2)
 
         # Add sample to replay buffer
         memory.add(s1, r, a, terminal)
 
         if step > 50:
             dream.train()
+
         if step > 50 and step % 500 == 0:
             dream.test()
 
