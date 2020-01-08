660c4fc26212365517ce83c50cb4ac086f0b49b4
Per-Arne Andersen <per@sysx.no>
2020-01-09 00:34:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='gym_maze',
       version='2.0.0',
-      install_requires=['gym', 'numpy', 'pygame', 'scikit-image'],
+      install_requires=['gym', 'numpy', 'pygame==2.0.0.dev6', 'scikit-image'],
       packages=['gym_maze', 'gym_maze.envs', 'cair_maze']
       )
